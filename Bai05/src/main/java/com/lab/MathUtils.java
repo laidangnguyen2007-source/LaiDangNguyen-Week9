9e41042f1e4070ada620fe2c44dde70b4d5e4e41
TobiKumo <laidangnguyen2007@gmail.com>
2026-04-30 22:03:07 +0700

    Add new features to MathUtils and trigger coverage failure

--- Bai05/src/main/java/com/lab/MathUtils.java
+++ Bai05/src/main/java/com/lab/MathUtils.java
@@ -1,7 +1,9 @@
 package com.lab;
 
 public class MathUtils {
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+    public int add(int a, int b) { return a + b; }
+    
+    // Hàm mới này chưa có Test nè
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
 }
